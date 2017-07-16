5a2a93129c2d659d3f80185388657e72e1889c4e
sedrubal <dev@sedrubal.de>
2017-07-16 22:55:26 +0200

    Initial Commit: Add custom validation for UltimateMember captcha

---
+++ README.md
@@ -0,0 +1,12 @@
+# WordPress FAU FabLab modifications
+
+WordPress mods and extensions which are not worth for a own plugin.
+
+## Contents
+
+- custom validation for the captcha field in register form for
+  [UltimateMember](https://github.com/ultimatemember/ultimatemember/)
+
+## License
+
+[CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
---
+++ fau-fablab-mods.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: FAU FabLab Mods
+ * Plugin URI: https://github.com/fau-fablab/wp-fau-fablab-mods/
+ * Description: WordPress modifications by the FAU FabLab which are not worth for a own plugin
+ * Version: 0.0.1
+ * Author: fau-fablab
+ * Author URI: https://github.com/fau-fablab/
+ * Network: false
+ * License: CC BY 4.0
+ */
+
+/**
+ * custom form field validation for [UltimateMember](https://github.com/ultimatemember/ultimatemember/)
+ * docs: http://docs.ultimatemember.com/article/94-apply-custom-validation-to-a-field
+ */
+add_action('um_submit_form_errors_hook_', 'um_custom_validate_captcha', 999, 1);
+function um_custom_validate_captcha( $args ) {
+	global $ultimatemember;
+	$fablab_captcha_name = 'fablab_captcha';
+	$fablab_captcha_solution = 'Erlangen';
+
+	if ( !isset( $args[$fablab_captcha_name] ) || $args[$fablab_captcha_name] !== $fablab_captcha_solution) {
+		$ultimatemember->form->add_error( $fablab_captcha_name, 'Diese Antwort ist leider falsch.' );
+	}
+}
